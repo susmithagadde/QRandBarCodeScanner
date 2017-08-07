51783765bd2344b9494b5f1116b6402d0506a5c7
susmithagadde <sushmitha.barbie@gmail.com>
2017-08-07 21:37:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrandbarcodescanner/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.qrandbarcodescanner;
+
+import android.database.CursorJoiner;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scanCode(View view){
+        scannerView = new ZXingScannerView(this);
+        scannerView.setResultHandler(new ZXingScannerResultHandler());
+
+        setContentView(scannerView);
+        scannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler
+    {
+        @Override
+        public void handleResult(Result result){
+          String resultCode = result.getText();
+            Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
+
+            setContentView(R.layout.activity_main);
+            scannerView.stopCamera();
+        }
+    }
+}
